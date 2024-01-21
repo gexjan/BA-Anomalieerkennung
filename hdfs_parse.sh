294eef6db6845617ea05a918bef1f1f6ba0278d0
Janik <74431854+gexjan@users.noreply.github.com>
2024-01-21 11:42:34 +0100

    Init

---
+++ hdfs_parse.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python main.py -prepare --dataset hdfs --log-dir ./logs/HDFS --log-file hdfs_200k.log
---
+++ postgres_parse.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python main.py -prepare --log-dir ./logs/033e --log-file postgresql-01.log
